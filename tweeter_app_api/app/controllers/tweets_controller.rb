dd08a73ea518d07b3ce46710cd06b0518a5d2d31
Sampreet-Chawla <sam.learnnew@gmail.com>
2020-11-11 09:14:27 -0500

    Commit 6: All Tweets are visible at the index route

--- tweeter_app_api/app/controllers/tweets_controller.rb
+++ tweeter_app_api/app/controllers/tweets_controller.rb
@@ -3,9 +3,10 @@ class TweetsController < ApplicationController
     def index
         begin
             render(status: 200, json: {tweets: Tweet.all})
-        rescue Exception => e
-            render(status: 500, json: {error: "Internal Server Error: #{e.message}"})
+        rescue => error
+            render(status: 500, json: {error: "Internal Server Error: #{error.message}"})
         end
+        
     end
 
     def show 
@@ -13,8 +14,8 @@ class TweetsController < ApplicationController
             render(status: 200, json: {tweet: Tweet.find(params[:id])})
         rescue ActiveRecord::RecordNotFound 
             render(status: 400, json: {error: "Tweet not found with id: #{params[:id]}"})
-        rescue  Exception => e
-            render(status: 500, json: {error: "Internal Server Error: #{e.message}"})
+        rescue  => error
+            render(status: 500, json: {error: "Internal Server Error: #{error.message}"})
         end
     end
 end
