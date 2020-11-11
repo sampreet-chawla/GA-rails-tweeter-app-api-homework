23bad38786e4dcf2bf69764513c28e1bddaa1a81
Sampreet-Chawla <sam.learnnew@gmail.com>
2020-11-11 09:33:17 -0500

    Commit 8: Can POST new Tweets

--- tweeter_app_api/app/controllers/tweets_controller.rb
+++ tweeter_app_api/app/controllers/tweets_controller.rb
@@ -16,6 +16,28 @@ class TweetsController < ApplicationController
         rescue  => error
             render(status: 500, json: {error: "Internal Server Error: #{error.message}"})
         end
-        
+
     end
+
+    # Tested with cURL expression below
+    # curl -X POST -d "tweet[title]=Crentist" -d "tweet[author]=Dwight" -d tweet[content]="My dentist's name is Crentist, maybe that's why he became a dentist" localhost:3000/tweets
+    def create
+        begin 
+            new_tweet = Tweet.new(tweet_params)
+            if new_tweet.save()
+                render(status: 201, json: {tweet: new_tweet})
+            else
+                render(status: 422, json: {error: new_tweet.errors})
+            end
+        rescue  => error
+            render(status: 500, json: {error: "Internal Server Error: #{error.message}"})
+        end
+    end
+
+    private 
+
+    def tweet_params # strong params for create
+        params.require(:tweet).permit(:title, :content, :author)
+    end
+
 end
