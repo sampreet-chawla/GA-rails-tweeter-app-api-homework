536e1de65f572a88723a7853fc1199dde794be47
Sampreet-Chawla <sam.learnnew@gmail.com>
2020-11-11 10:08:17 -0500

    Commit 9: Tweets update

--- tweeter_app_api/app/controllers/tweets_controller.rb
+++ tweeter_app_api/app/controllers/tweets_controller.rb
@@ -12,7 +12,7 @@ class TweetsController < ApplicationController
         begin
             render(status: 200, json: {tweet: Tweet.find(params[:id])})
         rescue ActiveRecord::RecordNotFound 
-            render(status: 400, json: {error: "Tweet not found with id: #{params[:id]}"})
+            render(status: 404, json: {error: "Tweet not found with id: #{params[:id]}"})
         rescue  => error
             render(status: 500, json: {error: "Internal Server Error: #{error.message}"})
         end
@@ -26,7 +26,7 @@ class TweetsController < ApplicationController
             new_tweet = Tweet.new(tweet_params)
             if new_tweet.save()
                 render(status: 201, json: {tweet: new_tweet})
-            else
+            else # if validation fails
                 render(status: 422, json: {error: new_tweet.errors})
             end
         rescue  => error
@@ -34,9 +34,21 @@ class TweetsController < ApplicationController
         end
     end
 
+    def update
+        begin 
+            found_tweet = Tweet.find(params[:id])
+            found_tweet.update(tweet_params)
+            render(status: 200, json: {tweet: found_tweet})               
+        rescue ActiveRecord::RecordNotFound 
+            render(status: 404, json: {error: "Tweet not found with id: #{params[:id]}"})
+        rescue  => error
+            render(status: 500, json: {error: "Internal Server Error: #{error.message}"})
+        end
+    end
+
     private 
 
-    def tweet_params # strong params for create
+    def tweet_params # strong params for create / update
         params.require(:tweet).permit(:title, :content, :author)
     end
 
--- tweeter_app_api/app/models/tweet.rb
+++ tweeter_app_api/app/models/tweet.rb
@@ -1,2 +1,3 @@
 class Tweet < ApplicationRecord
+    validates :title, :content, :author, presence: true
 end
